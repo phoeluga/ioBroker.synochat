ff29a3edf73965b6c8291df330946e8e6542d1b0
Julian Hassler <jhassler@outlook.com>
2022-04-03 14:57:55 +0200

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"synochat adapter settings": {
+		"en": "Adapter settings for synochat",
+		"de": "Adaptereinstellungen für synochat",
+		"ru": "Настройки адаптера для synochat",
+		"pt": "Configurações do adaptador para synochat",
+		"nl": "Adapterinstellingen voor synochat",
+		"fr": "Paramètres d'adaptateur pour synochat",
+		"it": "Impostazioni dell'adattatore per synochat",
+		"es": "Ajustes del adaptador para synochat",
+		"pl": "Ustawienia adaptera dla synochat",
+		"zh-cn": "synochat的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "вариант 2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	}
+};
