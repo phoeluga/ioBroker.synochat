f2f84955a6b266805fda52eab7793be75e68c21f
OlliMartin <scholl3ss93@gmail.com>
2025-04-04 11:11:41 +0200

    Reformat

--- main.test.js
+++ main.test.js
@@ -12,18 +12,17 @@ const { formatReceivedOnMessageData } = require("./template-interpolation.js");
 // import { functionToTest } from "./moduleToTest";
 
 describe("module to test => function to test", () => {
-	// initializing logic
-	const expected = 5;
+  // initializing logic
+  const expected = 5;
 
-	it(`should return ${expected}`, () => {
-		const result = 5;
-		// assign result a value from functionToTest
-		expect(result).to.equal(expected);
-		// or using the should() syntax
-		result.should.equal(expected);
-	});
-	// ... more tests => it
-
+  it(`should return ${expected}`, () => {
+    const result = 5;
+    // assign result a value from functionToTest
+    expect(result).to.equal(expected);
+    // or using the should() syntax
+    result.should.equal(expected);
+  });
+  // ... more tests => it
 });
 
 const DEFAULT_FROM = "from-propertery-top-level";
@@ -38,79 +37,156 @@ const DEFAULT_CATEGORY_NAME = "category name";
 const DEFAULT_DOTTED_PROPERTY = "this is a dotted property";
 const DEFAULT_ACTUAL_NESTED_PROPERTY = "this is an actual nested property";
 
+const DEFAULT_MESSAGE_OBJ = {
+  message: "Plant Ren is not having a perfect moisture status",
+  ts: 1743750514032,
+  contextData: {
+    name: "Ren",
+    detailedName: "Dracaena reflexa",
+    "dotted.name": DEFAULT_DOTTED_PROPERTY,
+    dotted: {
+      name: DEFAULT_ACTUAL_NESTED_PROPERTY,
+    },
+    actual: 3,
+  },
+};
+
 const DEFAULT_NOTIFICATION_MANAGER_MESSAGE = {
-	command: "command-tbd",
-	from: DEFAULT_FROM,
-	_id: DEFAULT_ID,
-	message: {
-		"host": DEFAULT_HOST,
-		"scope": {
-			"name": DEFAULT_SCOPE_NAME,
-			"description": "notification-description"
-		},
-		"test.nested.property": DEFAULT_DOTTED_PROPERTY,
-		"test": {
-			"nested": {
-				"property": DEFAULT_ACTUAL_NESTED_PROPERTY
-			}
-		},
-		"category": {
-			"instances": {
-				"system.adapter.adapter.0": {
-					"messages": [
-						{
-							"message": "Plant Ren is not having a perfect moisture status",
-							"ts": 1743750514032,
-							"contextData": {
-								"name": "Ren",
-								"sName": "Dracaena reflexa",
-								"actual": 3
-							}
-						}
-					]
-				}
-			},
-			"description": DEFAULT_CATEGORY_DESCRIPTION,
-			"name": DEFAULT_CATEGORY_NAME,
-			"severity": "alert"
-		}
-	}
+  command: "command-tbd",
+  from: DEFAULT_FROM,
+  _id: DEFAULT_ID,
+  message: {
+    host: DEFAULT_HOST,
+    scope: {
+      name: DEFAULT_SCOPE_NAME,
+      description: "notification-description",
+    },
+    "test.nested.property": DEFAULT_DOTTED_PROPERTY,
+    test: {
+      nested: {
+        property: DEFAULT_ACTUAL_NESTED_PROPERTY,
+      },
+    },
+    category: {
+      instances: {
+        "system.adapter.adapter.0": {
+          messages: [DEFAULT_MESSAGE_OBJ],
+        },
+      },
+      description: DEFAULT_CATEGORY_DESCRIPTION,
+      name: DEFAULT_CATEGORY_NAME,
+      severity: "alert",
+    },
+  },
 };
 
 describe("formatReceivedOnMessageData", () => {
-	it("should return template without parameters", () => {
-		const result = formatReceivedOnMessageData(DEFAULT_NOTIFICATION_MANAGER_MESSAGE, "Hello World");
+  it("should return template without parameters", () => {
+    const result = formatReceivedOnMessageData(
+      DEFAULT_NOTIFICATION_MANAGER_MESSAGE,
+      "Hello World",
+    );
 
-		expect(result).equals("Hello World");
-	});
-	
-	it("should skip processing contextData if not present in the notification", () => {
-		// TODO
-	});
-	
-	const replacementTests = [
-		{ args: ["${_id}"], expected: `${DEFAULT_ID}` },
-		{ args: ["${instances}"], expected: `adapter.0` },
-		{ args: ["${from}"], expected: `${DEFAULT_FROM}` },
-		{ args: ["${message.host}"], expected: `"${DEFAULT_HOST}"` },
+    expect(result).equals("Hello World");
+  });
 
-		{ args: ["${message.category.description}"], expected: `"${DEFAULT_CATEGORY_DESCRIPTION}"` },
-		{ args: ["${message.category.name}"], expected: `"${DEFAULT_CATEGORY_NAME}"` },
+  const replacementTests = [
+    { args: ["${_id}"], expected: `${DEFAULT_ID}` },
+    { args: ["${instances}"], expected: `adapter.0` },
+    { args: ["${from}"], expected: `${DEFAULT_FROM}` },
+    { args: ["${message.host}"], expected: `"${DEFAULT_HOST}"` },
 
-		// Check that dotted/nested properties are handled correctly
-		{ args: ["${message.test.nested.property}"], expected: `"${DEFAULT_ACTUAL_NESTED_PROPERTY}"` },
-		{ args: ["${message.test/-nested/-property}"], expected: `"${DEFAULT_DOTTED_PROPERTY}"` },
+    {
+      args: ["${message.category.description}"],
+      expected: `"${DEFAULT_CATEGORY_DESCRIPTION}"`,
+    },
+    {
+      args: ["${message.category.name}"],
+      expected: `"${DEFAULT_CATEGORY_NAME}"`,
+    },
 
-		{ args: ["${instances}: ${message.host}"], expected: `adapter.0: "${DEFAULT_HOST}"` },
+    // Check that dotted/nested properties are handled correctly
+    {
+      args: ["${message.test.nested.property}"],
+      expected: `"${DEFAULT_ACTUAL_NESTED_PROPERTY}"`,
+    },
+    {
+      args: ["${message.test/-nested/-property}"],
+      expected: `"${DEFAULT_DOTTED_PROPERTY}"`,
+    },
 
-		{ args: ["${contextData.name}"], expected: `"Ren"` },
-	];
+    {
+      args: ["${instances}: ${message.host}"],
+      expected: `adapter.0: "${DEFAULT_HOST}"`,
+    },
 
-	replacementTests.forEach(({args, expected}) => {
-		it(`correctly replaces ${args[0]}`, function () {
-			const res = formatReceivedOnMessageData(DEFAULT_NOTIFICATION_MANAGER_MESSAGE, args[0], { receivedNotificationManagerTemplate: args[0] });
-			expect(res).equals(expected);
-		});
-	});
+    { args: ["${contextData.name}"], expected: `"Ren"` },
+    {
+      args: ["${contextData.name} -> ${contextData.detailedName}"],
+      expected: `"Ren" -> "Dracaena reflexa"`,
+    },
+
+    {
+      args: ["${contextData.dotted.name}"],
+      expected: `"${DEFAULT_ACTUAL_NESTED_PROPERTY}"`,
+    },
+    {
+      args: ["${contextData.dotted/-name}"],
+      expected: `"${DEFAULT_DOTTED_PROPERTY}"`,
+    },
+  ];
+
+  replacementTests.forEach(({ args, expected }) => {
+    it(`correctly replaces ${args[0]}`, function () {
+      const res = formatReceivedOnMessageData(
+        DEFAULT_NOTIFICATION_MANAGER_MESSAGE,
+        args[0],
+        { receivedNotificationManagerTemplate: args[0] },
+      );
+      expect(res).equals(expected);
+    });
+  });
+
+  it("should skip processing contextData if not present in the notification", () => {
+    const notificationClone = {
+      ...DEFAULT_NOTIFICATION_MANAGER_MESSAGE,
+      message: {
+        ...DEFAULT_NOTIFICATION_MANAGER_MESSAGE.message,
+        category: {
+          ...DEFAULT_NOTIFICATION_MANAGER_MESSAGE.message.category,
+          instances: [],
+        },
+      },
+    };
+
+    const template = "${contextData.name}";
+    const result = formatReceivedOnMessageData(notificationClone, template, {
+      receivedNotificationManagerTemplate: template,
+    });
+    expect(result).equals("${contextData.name}");
+  });
+
+  it("should skip processing contextData if multiple messages are received", () => {
+    const notificationClone = {
+      ...DEFAULT_NOTIFICATION_MANAGER_MESSAGE,
+      message: {
+        ...DEFAULT_NOTIFICATION_MANAGER_MESSAGE.message,
+        category: {
+          ...DEFAULT_NOTIFICATION_MANAGER_MESSAGE.message.category,
+          instances: {
+            ...DEFAULT_NOTIFICATION_MANAGER_MESSAGE.message.category.instances,
+            "system.adapter.adapter.0": {
+              messages: [DEFAULT_MESSAGE_OBJ, DEFAULT_MESSAGE_OBJ],
+            },
+          },
+        },
+      },
+    };
+
+    const template = "${contextData.name}";
+    const result = formatReceivedOnMessageData(notificationClone, template, {
+      receivedNotificationManagerTemplate: template,
+    });
+    expect(result).equals("${contextData.name}");
+  });
 });
-
--- template-interpolation.js
+++ template-interpolation.js
@@ -1,70 +1,86 @@
-function replacePropertiesForTemplate(formattedMessage, templateString, dataSource, log) {
-    let maxItter = 100000;
-    const regexStr = `\\$\\{${templateString}\\.(.+?)\\}`;
-    const regex = new RegExp(regexStr);
-    
-    while (regex.test(formattedMessage) && maxItter > 0) {
-        const match = regex.exec(formattedMessage);
-        if(!match) break;
-        
-        let replacePattern = match[1];
-        let replaceValue = replacePattern.split(".").reduce(function (o, k) {
-            return o && o[k.replaceAll("/-", ".")];
-        }, dataSource);
-        formattedMessage = formattedMessage.replaceAll(
-            String(`\${message.${replacePattern}}`),
-            JSON.stringify(replaceValue),
-        );
+function replacePropertiesForTemplate(
+  formattedMessage,
+  templateString,
+  dataSource,
+  log,
+) {
+  let maxItter = 100000;
+  const regexStr = `\\$\\{${templateString}\\.(.+?)\\}`;
+  const regex = new RegExp(regexStr);
 
-        maxItter--;
-    }
+  while (regex.test(formattedMessage) && maxItter > 0) {
+    const match = regex.exec(formattedMessage);
+    if (!match) break;
 
-    if (maxItter <= 0 && !!log) {
-        log.error(
-            `Infinite loop while parsing JSON detected! Returning raw text for ${templateString}!`,
-        );
-        formattedMessage = dataSource;
-    }
-    
-    return formattedMessage;
+    let replacePattern = match[1];
+    let replaceValue = replacePattern.split(".").reduce(function (o, k) {
+      return o && o[k.replaceAll("/-", ".")];
+    }, dataSource);
+
+    formattedMessage = formattedMessage.replaceAll(
+      String(`\${${templateString}.${replacePattern}}`),
+      JSON.stringify(replaceValue),
+    );
+
+    maxItter--;
+  }
+
+  if (maxItter <= 0 && !!log) {
+    log.error(
+      `Infinite loop while parsing JSON detected! Returning raw text for ${templateString}!`,
+    );
+    formattedMessage = dataSource;
+  }
+
+  return formattedMessage;
 }
 
 function formatReceivedOnMessageData(obj, formatTemplate, config, log) {
-    let formattedMessage = formatTemplate;
+  let formattedMessage = formatTemplate;
 
-    if (formatTemplate === config?.receivedNotificationManagerTemplate) {
-        const { instances } = obj.message.category;
-        const readableInstances = Object.entries(instances).map(
-            ([instance, _entry]) =>
-                `${instance.substring("system.adapter.".length)}`,
-        );
-        formattedMessage = formattedMessage.replaceAll(
-            "${instances}",
-            String(readableInstances.join(", ")),
-        );
-        
-        const allMessages = Object.values(instances).map(byAdapter => byAdapter.messages ?? [])
-            .reduce((prev, curr) => [...prev, ...curr], []);
-        
-        if(allMessages.length === 1 && !!allMessages[0].contextData) {
-            formatTemplate = replacePropertiesForTemplate(formattedMessage, "contextData", allMessages[0].contextData, log);
-        }
+  if (formatTemplate === config?.receivedNotificationManagerTemplate) {
+    const { instances } = obj.message.category;
+    const readableInstances = Object.entries(instances).map(
+      ([instance, _entry]) => `${instance.substring("system.adapter.".length)}`,
+    );
+    formattedMessage = formattedMessage.replaceAll(
+      "${instances}",
+      String(readableInstances.join(", ")),
+    );
+
+    const allMessages = Object.values(instances)
+      .map((byAdapter) => byAdapter.messages ?? [])
+      .reduce((prev, curr) => [...prev, ...curr], []);
+
+    if (allMessages.length === 1 && !!allMessages[0].contextData) {
+      formattedMessage = replacePropertiesForTemplate(
+        formattedMessage,
+        "contextData",
+        allMessages[0].contextData,
+        log,
+      );
     }
+  }
 
-    formattedMessage = formattedMessage.replaceAll(
-        "${command}",
-        String(obj.command),
-    );
-    formattedMessage = formattedMessage.replaceAll("${from}", String(obj.from));
-    formattedMessage = formattedMessage.replaceAll("${_id}", String(obj._id));
-    formattedMessage = formattedMessage.replaceAll(
-        "${message}",
-        String(JSON.stringify(obj.message, undefined, 4)),
-    );
+  formattedMessage = formattedMessage.replaceAll(
+    "${command}",
+    String(obj.command),
+  );
+  formattedMessage = formattedMessage.replaceAll("${from}", String(obj.from));
+  formattedMessage = formattedMessage.replaceAll("${_id}", String(obj._id));
+  formattedMessage = formattedMessage.replaceAll(
+    "${message}",
+    String(JSON.stringify(obj.message, undefined, 4)),
+  );
 
-    formattedMessage = replacePropertiesForTemplate(formattedMessage, "message", obj.message, log);
-        
-    return formattedMessage;
+  formattedMessage = replacePropertiesForTemplate(
+    formattedMessage,
+    "message",
+    obj.message,
+    log,
+  );
+
+  return formattedMessage;
 }
 
 module.exports = { formatReceivedOnMessageData };
