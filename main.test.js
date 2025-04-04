0ba0dae73cc6119e8fdb879cdd0bbcb3a4646cbf
OlliMartin <scholl3ss93@gmail.com>
2025-04-04 10:29:38 +0200

    Add baseline test cases/tests for existing|unchanged code

--- main.test.js
+++ main.test.js
@@ -26,31 +26,54 @@ describe("module to test => function to test", () => {
 
 });
 
+const DEFAULT_FROM = "from-propertery-top-level";
+const DEFAULT_HOST = "iobroker-host";
+const DEFAULT_ID = "some-random-id";
+
+const DEFAULT_SCOPE_NAME = "scope name";
+
+const DEFAULT_CATEGORY_DESCRIPTION = "category description";
+const DEFAULT_CATEGORY_NAME = "category name";
+
+const DEFAULT_DOTTED_PROPERTY = "this is a dotted property";
+const DEFAULT_ACTUAL_NESTED_PROPERTY = "this is an actual nested property";
+
 const DEFAULT_NOTIFICATION_MANAGER_MESSAGE = {
-	"host": "system.host.dev-fyta-oma-desktop",
-	"scope": {
-		"name": "Fyta",
-		"description": "TODO"
-	},
-	"category": {
-		"instances": {
-			"system.adapter.fyta.0": {
-				"messages": [
-					{
-						"message": "Plant Ren is not having a perfect moisture status",
-						"ts": 1743750514032,
-						"contextData": {
-							"name": "Ren",
-							"sName": "Dracaena reflexa",
-							"actual": 3
-						}
-					}
-				]
+	command: "command-tbd",
+	from: DEFAULT_FROM,
+	_id: DEFAULT_ID,
+	message: {
+		"host": DEFAULT_HOST,
+		"scope": {
+			"name": DEFAULT_SCOPE_NAME,
+			"description": "notification-description"
+		},
+		"test.nested.property": DEFAULT_DOTTED_PROPERTY,
+		"test": {
+			"nested": {
+				"property": DEFAULT_ACTUAL_NESTED_PROPERTY
 			}
 		},
-		"description": "The humidity of a plant needs to be checked.",
-		"name": "Check Humidity",
-		"severity": "alert"
+		"category": {
+			"instances": {
+				"system.adapter.adapter.0": {
+					"messages": [
+						{
+							"message": "Plant Ren is not having a perfect moisture status",
+							"ts": 1743750514032,
+							"contextData": {
+								"name": "Ren",
+								"sName": "Dracaena reflexa",
+								"actual": 3
+							}
+						}
+					]
+				}
+			},
+			"description": DEFAULT_CATEGORY_DESCRIPTION,
+			"name": DEFAULT_CATEGORY_NAME,
+			"severity": "alert"
+		}
 	}
 };
 
@@ -59,6 +82,27 @@ describe("formatReceivedOnMessageData", () => {
 		const result = formatReceivedOnMessageData(DEFAULT_NOTIFICATION_MANAGER_MESSAGE, "Hello World");
 
 		expect(result).equals("Hello World");
+	});
+	
+	const singleReplacementTests = [
+		{ args: ["${_id}"], expected: `${DEFAULT_ID}` },
+		{ args: ["${instances}"], expected: `adapter.0` },
+		{ args: ["${from}"], expected: `${DEFAULT_FROM}` },
+		{ args: ["${message.host}"], expected: `"${DEFAULT_HOST}"` },
+
+		{ args: ["${message.category.description}"], expected: `"${DEFAULT_CATEGORY_DESCRIPTION}"` },
+		{ args: ["${message.category.name}"], expected: `"${DEFAULT_CATEGORY_NAME}"` },
+
+		// Check that dotted/nested properties are handled correctly
+		{ args: ["${message.test.nested.property}"], expected: `"${DEFAULT_ACTUAL_NESTED_PROPERTY}"` },
+		{ args: ["${message.test/-nested/-property}"], expected: `"${DEFAULT_DOTTED_PROPERTY}"` },
+	];
+
+	singleReplacementTests.forEach(({args, expected}) => {
+		it(`correctly replaces ${args[0]}`, function () {
+			const res = formatReceivedOnMessageData(DEFAULT_NOTIFICATION_MANAGER_MESSAGE, args[0], { receivedNotificationManagerTemplate: args[0] });
+			expect(res).equals(expected);
+		});
 	});
 });
 
